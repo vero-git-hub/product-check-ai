6028be1ec7ef1ef54da4c70d6ec95b07a3c81ee6
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-11-26 14:26:27 +0200

    Initial

---
+++ src/main/java/org/example/productcheckai/ProductCheckAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.productcheckai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCheckAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCheckAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/productcheckai/ProductCheckAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productcheckai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCheckAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
